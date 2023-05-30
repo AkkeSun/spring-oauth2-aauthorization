9656ae271e7a0eaa02a4501bcf90f46d1cd705bc
od <od@sweettracker.co.kr>
2023-05-30 18:56:29 +0900

    first commit

---
+++ src/test/java/com/example/springoauth2authorization/SpringOauth2AuthorizationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springoauth2authorization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringOauth2AuthorizationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
