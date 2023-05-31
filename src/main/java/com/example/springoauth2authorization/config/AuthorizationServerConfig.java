73be518e7967533a24d231d2a5a73979bd9ca9f6
od <od@sweettracker.co.kr>
2023-05-31 13:56:38 +0900

    perf: 엔드포인트 커스텀을 위한 설정 파일 수정

--- src/main/java/com/example/springoauth2authorization/config/AuthorizationServerConfig.java
+++ src/main/java/com/example/springoauth2authorization/config/AuthorizationServerConfig.java
@@ -3,10 +3,11 @@ package com.example.springoauth2authorization.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.authorization.OAuth2AuthorizationServerConfigurer;
 import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
+import org.springframework.security.web.util.matcher.RequestMatcher;
 
 /*
     인가서버 설정
@@ -16,7 +17,21 @@ public class AuthorizationServerConfig {
 
     @Bean
     public SecurityFilterChain authSecurityFilterChain(HttpSecurity http) throws Exception {
-        OAuth2AuthorizationServerConfiguration.applyDefaultSecurity(http);
+        OAuth2AuthorizationServerConfigurer<HttpSecurity> auth2AuthorizationServerConfigurer =
+            new OAuth2AuthorizationServerConfigurer<>();
+        RequestMatcher endpointMather = auth2AuthorizationServerConfigurer
+            .getEndpointsMatcher();
+
+        http
+            .requestMatcher(endpointMather)
+            .authorizeRequests(authorizationRequests ->
+                authorizationRequests.anyRequest().authenticated()
+            )
+            .csrf(csrf -> csrf.ignoringRequestMatchers(endpointMather))
+            .apply(auth2AuthorizationServerConfigurer);
+
+        // 단순 설정시 사용
+        // OAuth2AuthorizationServerConfiguration.applyDefaultSecurity(http);
         // 미인증시 /login 페이지 이동
         http.exceptionHandling(e -> e.authenticationEntryPoint(new LoginUrlAuthenticationEntryPoint("/login")));
         // access token 검증 API
